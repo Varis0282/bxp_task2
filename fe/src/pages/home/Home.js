3c353f3b3ebfca0b93eed81172812530d56ee43f
Varis0282 <varisrajakforth11@gmail.com>
2024-08-31 02:14:21 +0530

    First

---
+++ fe/src/pages/home/Home.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { Navbar } from '../../components'
+import { Button, message, Table } from 'antd';
+import { BaseURL } from '../../config';
+import axios from 'axios';
+
+const Home = () => {
+
+  const [authorized, setAuthorized] = useState(false);
+  const [emails, setEmails] = useState([])
+  const [open, setOpen] = useState(false)
+
+  const handleAuthorize = async () => {
+    try {
+      const { data } = await axios.get(`${BaseURL}/`);
+      const popupWidth = 600;
+      const popupHeight = 400;
+      const left = (window.screen.width / 2) - (popupWidth / 2);
+      const top = (window.screen.height / 2) - (popupHeight / 2);
+      const popup = window.open(data.data, '_blank', `width=${popupWidth},height=${popupHeight},top=${top},left=${left}`);
+      setOpen(true)
+
+      const checkPopupClosed = setInterval(() => {
+        if (popup?.closed) {
+          setOpen(false)
+          clearInterval(checkPopupClosed);
+          setAuthorized(true)
+          message.success('Authorization Successful');
+        }
+      }, 1000);
+    } catch (error) {
+      setAuthorized(false)
+      message.error('Authorization Failed');
+      console.error(error);
+    }
+  }
+
+  const getEmails = async () => {
+    try {
+      const { data } = await axios.get(`${BaseURL}/emails`);
+      if (data.success) {
+        console.log("🚀 => data.data:", data.data);
+        setEmails(data.data)
+      }
+    } catch (error) {
+      message.error('Failed to fetch emails');
+      console.error(error);
+    }
+  }
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get(`${BaseURL}/authorized`);
+      console.log("🚀 => data:", data);
+      if (data.data) {
+        setAuthorized(true)
+      } else {
+        setAuthorized(false)
+        handleAuthorize()
+      }
+    } catch (error) {
+      setAuthorized(false)
+      message.error('Failed to fetch emails');
+      console.error(error);
+    }
+  }
+
+  const columns = [
+    {
+      title: 'From',
+      dataIndex: 'from',
+      key: 'from',
+      width: '50%',
+      align: 'center'
+    },
+    {
+      title: 'Subject',
+      dataIndex: 'subject',
+      key: 'subject',
+      width: '50%',
+      align: 'center'
+    },
+  ];
+
+  useEffect(() => {
+    if (!open) {
+      checkAuth();
+    }
+  }, [open]);  //eslint-disable-line
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center gap-4 justify-center m-12 py-8 border rounded-lg">
+        <Button onClick={checkAuth}>{authorized ? 'Refresh' : 'Authorize'}</Button>
+        <Button onClick={getEmails}>{'Get Your Mails'}</Button>
+      </div>
+      <div className="flex items-center gap-4 justify-center m-12 py-8">
+        <Table columns={columns} dataSource={emails} bordered className='w-full'/>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const { OAuth2Client } = require('google-auth-library');
+const { google } = require('googleapis');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+
+// Load credentials from a file
+app.use(cors());
+app.use(express.json());
+const credentials = JSON.parse(fs.readFileSync('cred.json'));
+
+// Extract the necessary credentials
+const { client_id, client_secret, redirect_uris } = credentials.installed;
+const redirectUri = redirect_uris[0];
+
+// Create an OAuth2 client
+const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirectUri
+);;
+
+// Set up the authorization URL
+const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+});
+
+
+app.get('/', (req, res) => {
+    res.json({ success: true, error: false, type: 'success', data: authUrl });
+});
+
+// api to check authorized or not
+app.get('/authorized', (req, res) => {
+    if (oAuth2Client.credentials.access_token) {
+        res.json({ success: true, error: false, type: 'success', data: true });
+    } else {
+        res.json({ success: true, error: false, type: 'success', data: false });
+    }
+});
+
+app.get('/oauth2callback', async (req, res) => {
+    const code = req.query.code;
+    try {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        res.send('<h1>Authentication Success ! Please close the window</h1>');
+    } catch (err) {
+        console.error('Error retrieving access token', err);
+        res.send('<h1>Authentication Failure ! Please close the window</h1>');
+    }
+});
+
+app.get('/emails', async (req, res) => {
+    try {
+        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+        const response = await gmail.users.messages.list({
+            userId: 'me',
+            q: '-from:me',
+        });
+
+        const messages = response.data.messages || [];
+        const emailDetails = await Promise.all(
+            messages.map(async (message) => {
+                const msgResponse = await gmail.users.messages.get({
+                    userId: 'me',
+                    id: message.id,
+                });
+                const headers = msgResponse.data.payload.headers;
+                const subject = headers.find((header) => header.name === 'Subject').value;
+                const from = headers.find((header) => header.name === 'From').value;
+                return { from, subject };
+            })
+        );
+
+        res.json({ success: true, error: false, type: 'success', data: emailDetails });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
